6c0612f324e0610b29eb2aef4d9e19fdff5c84c3
Katiúscia Lopes <144278645+KatyLopes@users.noreply.github.com>
2024-01-25 23:02:13 +0000

    Ajustes e Melhorias-Desafio DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,24 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BÁSICO, INTERMEDIÁRIO, AVANÇADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Usuario(val nome: String, val email: String,  val duracao: Int = 60)
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
     }
+fun registrarUsuario(usuario: Usuario) {
+    val inscritos = inscritos.add(usuario)
+ 
+    println("Novo usuário: ${usuario.nome} adicionado com sucesso!")
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun novouruaio() {
+     // Create a new user
+    val usuario = Usuario("Katy Lopes", "katy@teste.com", "BÁSICO")
+
+    // Register the new user
+    registrarUsuario(usuario)
 }
+
